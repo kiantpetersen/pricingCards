50dffae0d3abfcfd34693d30cc8f77fc7b79dcb8
unknown <kiantpetersen@gmail.com>
2023-08-26 21:09:00 +0200

    adding media queries

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import './queries.css'
 import PriceCard from './PriceCard'
 
 function App() {
