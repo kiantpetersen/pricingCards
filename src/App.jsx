e1232079fb7110d1db27b423f2c4875b9c95f88c
unknown <kiantpetersen@gmail.com>
2023-08-26 20:49:27 +0200

    commiting before adding queries

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,40 @@ import PriceCard from './PriceCard'
 
 function App() {
   // const [count, setCount] = useState(0)
-  const [toggleVal, setToggle] = useState(false)
+  const data = [
+    {
+      title: 'Basic',
+      month: '$19.99',
+      year: '$191.99',
+      perks: ['500 GB Storage', '2 Users Allowed', 'Send up to 3 GB']
+    },
+    {
+      title: 'Professional',
+      month: '$24.99',
+      year: '$239.99',
+      perks: ['1 TB Storage', '5 Users Allowed', 'Send up to 10 GB']
+    },
+    {
+      title: 'Master',
+      month: '$39.99',
+      year: '$374.99',
+      perks: ['2 TB Storage', '10 Users Allowed', 'Send up to 20 GB']
+    },
+  ]
+  const [prices, setPrices] = useState([data[0].year, data[1].year, data[2].year])
+
+  const [toggleVal, setToggle] = useState(true)
 
   function handleClick(e) {
-    console.log('Event: ', e.target.value)
-    setToggle(prev => !prev)
+    // console.log('Event: ', e.target.value)
+    if (toggleVal === true) {
+      setPrices([data[0].month, data[1].month, data[2].month])
+      setToggle(prev => !prev)
+    } else {
+      setPrices([data[0].year, data[1].year, data[2].year])
+      setToggle(prev => !prev)
+    }
+
   }
 
   return (
@@ -25,9 +54,9 @@ function App() {
         <p className='pricing-toggle-text'>Monthly</p>
       </div>
       <div className='price-grid'>
-        <PriceCard isPrimary={false} />
-        <PriceCard isPrimary={true} />
-        <PriceCard isPrimary={false} />
+        <PriceCard price={prices[0]} data={data[0]} isPrimary={false} />
+        <PriceCard price={prices[1]} data={data[1]} isPrimary={true} />
+        <PriceCard price={prices[2]} data={data[2]} isPrimary={false} />
       </div>
 
     </div>
--- src/PriceCard.jsx
+++ src/PriceCard.jsx
@@ -1,30 +1,36 @@
 import React from 'react';
+import { v4 as uuidv4 } from 'uuid';
 
 function PriceCard(props) {
+    function getPerks() {
+        return props.data.perks.map((perk) => {
+            return <p key={uuidv4()} className='price-text'>{perk}</p>
+
+        })
+
+    }
     function getCard() {
         let res
         if (props.isPrimary === false) {
             res = <div className='price-card'>
-                <p className='pricing-title'>Basic</p>
-                <p className='pricing-amount'>$19.99</p>
+                <p className='pricing-title'>{props.data.title}</p>
+                <p className='pricing-amount'>{props.price}</p>
                 <div className='price-text-container'>
-                    <p className='price-text'>500 GB Storage</p>
-                    <p className='price-text'>2 Users Allowed</p>
-                    <p className='price-text'>Send up to 3 GB</p>
+                    {getPerks()}
+                    {/* <p className='price-text'>2 Users Allowed</p>
+                    <p className='price-text'>Send up to 3 GB</p> */}
                 </div>
                 <button className='btn price-btn'>Learn More</button>
 
             </div>
         } else {
             res = <div className='price-card primary-card'>
-                <p className='pricing-title'>Basic</p>
-                <p className='pricing-amount'>$19.99</p>
+                <p className='pricing-title'>{props.data.title}</p>
+                <p className='pricing-amount'>{props.price}</p>
                 <div className='price-text-container'>
-                    <p className='price-text'>500 GB Storage</p>
-                    <p className='price-text'>2 Users Allowed</p>
-                    <p className='price-text'>Send up to 3 GB</p>
+                    {getPerks()}
                 </div>
-                <button className='btn price-btn price-btn-primary'>Learn More</button>
+                <button className='btn price-btn price-btn-primary '>Learn More</button>
 
             </div>
         }
